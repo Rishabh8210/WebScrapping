808bf0886102166d3e9209100314d288adf404b7
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-09-10 22:48:29 +0530

    Fact : Added functionality to call findJobs() every 10 minutes in an infinite loop

--- FetchDataFromOthersWebsite.py
+++ FetchDataFromOthersWebsite.py
@@ -1,27 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35'
-htmlText = requests.get(url).text
+def findJobs():
+    url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35'
+    htmlText = requests.get(url).text
+    soup = BeautifulSoup(htmlText, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    print(f'Total {len(jobs)} - jobs found\n')
+    i = 1
+    for job in jobs:
+        jobInfo = job.find('h2')
+        jobDescInfo = job.find('ul', class_ = "list-job-dtl clearfix")
 
-soup = BeautifulSoup(htmlText, 'lxml')
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+        jobCompanyName = job.find('h3', class_ = "joblist-comp-name").text.strip().split('\n')
+        jobReq = job.find('ul', class_="top-jd-dtl clearfix")
 
-jobInfo = job.find('h2')
-jobDescInfo = job.find('ul', class_ = "list-job-dtl clearfix").li.text.strip().split('\n')
+        jobExp = jobReq.find_all('li')
+        keySkillsList = job.find('span', class_ = "srp-skills").text.strip().split(',')
+        keySkillStr = ""
+        for skill in keySkillsList:
+            keySkillStr += skill.strip()
+            keySkillStr += ' '
 
-jobCompanyName = job.find('h3', class_ = "joblist-comp-name").text.strip().split('\n')
-jobReq = job.find('ul', class_="top-jd-dtl clearfix")
+        jobDate = job.find('span', class_ = "sim-posted").text.strip()
+        forMoreDetail = jobDescInfo.find('a').get('href')
+        print(f"{i} - Job information")
+        print(f'Status : {jobDate}')
+        print(f'Job Name : {jobInfo.text.strip()}')
+        print(f'Company Name : {jobCompanyName[0]}')
+        print(f'Skills Required : {keySkillStr}')
+        print(f'For more details click here {forMoreDetail}')
+        print("\n")
+        i = i + 1
 
-jobExp = jobReq.find_all('li')
-keySkillsList = job.find('span', class_ = "srp-skills").text.strip().split(',')
-keySkillStr = ""
-for skill in keySkillsList:
-    keySkillStr += skill.strip()
-    keySkillStr += ' '
 
-print(keySkillStr)
-print('Job Information')
-print(f'Job Name : {jobInfo.text.strip()}')
-print(f'Company Name : {jobCompanyName[0]}')
-print(f'Skills Required : {keySkillStr}')
+if(__name__ == '__main__'):
+    while True:
+        findJobs()
+        # Call the findJobs() function every 10 minutes in an infinite loop
+        time.sleep(600)
+
